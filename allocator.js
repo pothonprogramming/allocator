36b397927860ee8ca08be7f63b84ae39aee22d43
Frank <58096114+pothonprogramming@users.noreply.github.com>
2025-10-06 18:05:30 -0400

    slightly more code

--- allocator.js
+++ allocator.js
@@ -1,9 +1,27 @@
+//***************/
+// THINGS TO DO //
+/****************/
+// * Build a basic buffer with a header, metadata, and data.
+
+//////////////////////
+// ABOUNT ALLOCATOR //
+//////////////////////
+// This application is intended to showcase the Buffer utility.
 const Allocator = (() => {
+
+  const HEADER_INDICES = {
+    headerLength:0,
+  }
 
   const BUFFER_LENGTH = 1000
   const buffer = new Uint32Array(BUFFER_LENGTH)
 
-  
+  // * Even if a buffer is "self-describing", the application it is loaded into still needs to know how to read it.
+  // There will always have to be some hard coded mechanism that allows you to interpret the data in the buffer.
+ 
+  // A header can be used to describe the rest of the application's metadata.
+  buffer[HEADER_INDICES.headerLength] = 1;
+
 
   return {
     initialize() {
--- library/buffer.js
+++ library/buffer.js
@@ -1,3 +1,8 @@
+//////////////////
+// ABOUT BUFFER //
+//////////////////
+// * Buffer is intended to be an extremely generic, stateless, buffer allocation utility.
+// The goal of Buffer is to assist with the creation of structured byte arrays and custom memory management.
 const Buffer = {
 
   
