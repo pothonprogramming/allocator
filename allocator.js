21c305de234b9d4ae8af8c0ea36f69955e062e94
Frank <58096114+pothonprogramming@users.noreply.github.com>
2025-10-05 21:09:00 -0400

    adding some code

---
+++ allocator.js
@@ -0,0 +1,13 @@
+const Allocator = (() => {
+
+  const BUFFER_LENGTH = 1000
+  const buffer = new Uint32Array(BUFFER_LENGTH)
+
+  
+
+  return {
+    initialize() {
+      
+    }
+  }
+})()
--- platform/browser-interface.js
+++ platform/browser-interface.js
@@ -1,1 +1,6 @@
 // all browser specific code should go here
+(() => {
+  console.log("hello")
+
+  Allocator.initialize()
+})()
