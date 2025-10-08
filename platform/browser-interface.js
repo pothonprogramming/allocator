8cb43e602f05ab5c68129dfd49708ea7c6f3ff21
Frank <58096114+pothonprogramming@users.noreply.github.com>
2025-10-08 14:52:50 -0400

    messing with output p element

--- platform/browser-interface.js
+++ platform/browser-interface.js
@@ -4,7 +4,8 @@
 // * All browser specific code should go here.
 // This is essentially the "glue" layer that connects the more generic application code to the browser platform.
 (() => {
-  const OUTPUT = document.getElementById("output");
+  const OUTPUT = document.createElement("p");
+  document.body.append(OUTPUT);
 
   OUTPUT.innerText = "Hello";
 
